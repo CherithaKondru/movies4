c2ddcd183d08d1def0de2d27ac098c62b2c8a7f1
CherithaKondru <cherithakondru8@gmail.com>
2024-12-01 23:39:18 +0530

    Android app

---
+++ app/src/main/java/com/example/movies4/Domain/GenresItem.java
@@ -0,0 +1,22 @@
+package com.example.movies4.Domain;
+
+public class GenresItem {
+    private String name;
+    private Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
